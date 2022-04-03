7a7542f2f4c1a9acd35c7a5ec50ee2b7f4c8cb34
Muharrem Köroğlu <68854616+MuharremKoroglu@users.noreply.github.com>
2022-04-03 13:53:10 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:ilginc_bilgiler/test_veri.dart';
+import 'constants.dart';
+
+void main() => runApp(IlgincSorular());
+
+class IlgincSorular extends StatelessWidget {
+  const IlgincSorular({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.purple[900],
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: SoruSayfasi(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SoruSayfasi extends StatefulWidget {
+  const SoruSayfasi({Key? key}) : super(key: key);
+  @override
+  State<SoruSayfasi> createState() => _SoruSayfasiState();
+}
+
+class _SoruSayfasiState extends State<SoruSayfasi> {
+  List<Widget> yuzIfadeleri = [];
+
+  TestVeri test_1 = TestVeri();
+
+  void butonFonkiyonu(bool butonDegeri) {
+    if (test_1.soruBitti() == true) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: new Text(
+              "Tebrikler! Testi Bitirdiniz.",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            content: new Text(
+              "Yeniden denemek için lüfen butona tıklayınız.",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            actions: <Widget>[
+              // usually buttons at the bottom of the dialog
+              new ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.purple),
+                ),
+                child: new Text(
+                  "Tekrar Dene",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  setState(() {
+                    test_1.testiSifirla();
+                    yuzIfadeleri = [];
+                  });
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      setState(() {
+        test_1.getSoruYaniti() == butonDegeri
+            ? yuzIfadeleri.add(kdogruYuzIfadesi)
+            : yuzIfadeleri.add(kyanlisYuzIfadesi);
+        test_1.soruArttir();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                test_1.getSoruMetni(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Wrap(
+          spacing: 15,
+          runSpacing: 3,
+          children: yuzIfadeleri,
+        ),
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 5.0,
+                    ),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.red),
+                      ),
+                      onPressed: () {
+                        butonFonkiyonu(false);
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Icon(
+                          Icons.thumb_down,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green),
+                      ),
+                      onPressed: () {
+                        butonFonkiyonu(true);
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Icon(
+                          Icons.thumb_up,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
